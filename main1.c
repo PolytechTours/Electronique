e43047b9ffe3ea1bec586f318218af7595a740c9
Aatrick <113598245+Aatrick@users.noreply.github.com>
2023-11-10 16:43:11 +0100

    Add Bluetooth functionality using SoftwareSerial library

--- main1.c
+++ main1.c
@@ -1,9 +1,17 @@
+#include <SoftwareSerial.h>
+#define rxPin 11 // Broche 11 en tant que RX, à raccorder sur TX du HC-05
+#define txPin 10 // Broche 10 en tant que TX, à raccorder sur RX du HC-05
+SoftwareSerial mySerial(rxPin, txPin);
+
 const int TransistorPin = 11;
 const int TMPin = 23;
 
 void setup()
 {
-    Serial.begin(9600);  
+    pinMode(rxPin, INPUT);
+    pinMode(txPin, OUTPUT);
+    mySerial.begin(38400);
+    Serial.begin(38400);  
     pinMode(TransistorPin, OUTPUT);
     pinMode(TMPin, INPUT);
 }
@@ -38,3 +46,19 @@ void loop()
         digitalWrite(TransistorPin, 1023);
     }
 }
+
+//void loop() //BLE
+//{
+//    int i = 0;
+//    char someChar[32] = {0};
+//    if(Serial.available()) {
+//    do{
+//    someChar[i++] = Serial.read();
+//    delay(3);
+//    }while (Serial.available() > 0);
+//    mySerial.println(someChar);
+//    Serial.println(someChar);
+//    }
+//    while(mySerial.available())
+//    Serial.print((char)mySerial.read());
+//}
